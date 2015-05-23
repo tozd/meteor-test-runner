b6d3123162cc2b57a5a090caf08b5eadd91fcab4
Jernej Kos <jernej@kos.mx>
2015-05-23 18:17:10 +0200

    Wait for Meteor to be ready before starting tests.

--- test-package.sh
+++ test-package.sh
@@ -11,16 +11,30 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 # Spawn the test process.
 echo "  > Spawning test process..."
-meteor test-packages --once --driver-package 'test-in-console' -p 4096 ${PACKAGE} >/dev/null 2>/dev/null &
-METEOR_PID=$!
-# Ensure that the process is killed on exit.
-trap "kill ${METEOR_PID}" EXIT
-
-echo "  > Waiting for Meteor to initialize..."
-sleep 15
+# Redirect standard output to file descriptor #3 so we can wait for Meteor to get ready.
+{ exec 3< <(meteor test-packages --once --driver-package 'test-in-console' -p 4096 ${PACKAGE}) ; } 2>/dev/null
+# Wait until Meteor reports that it is 'listening'.
+echo "  > Waiting for Meteor to start..."
+while read line; do
+   case "$line" in
+   *listening*)
+      echo "  > Meteor seems ready, going to run tests in a moment."
+      sleep 5
+      break
+      ;;
+   *)
+      ;;
+   esac
+done <&3
 
 # Run the test.
 echo "  > Running the test runner..."
 node tests/runner.js
-exit $?
+RESULT=$?
+
+# Close the file descriptor.
+exec 3<&-
+killall node 2>/dev/null
+wait 2>/dev/null
+exit $RESULT
 
