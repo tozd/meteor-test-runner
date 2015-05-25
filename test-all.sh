a4d6be07a0f3d82f38d93f50474dbdf8827d0635
Mitar <mitar.git@tnode.com>
2015-05-25 15:15:47 -0700

    Made test work also from a subdirectory.

--- test-all.sh
+++ test-all.sh
@@ -3,15 +3,14 @@
 # Packages with tests. To ignore a package, place // NOTEST after the onTest definition.
 PACKAGES=$(grep -Prl 'Package\.(onTest|on_test)(?!.*// NOTEST)' packages | grep -Po 'packages/\K.*(?=/)' | sort -u)
 
-# Move to the correct directory.
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
 # Perform tests.
 TESTS_FAILED=0
 PACKAGES_FAILED=""
 for pkg in ${PACKAGES}; do
   echo ">>> Testing package '${pkg}'..."
-  ./tests/test-package.sh packages/${pkg} || {
+  "${SCRIPT_DIR}/test-package.sh" packages/${pkg} || {
     echo "ERROR: Tests for package '${pkg}' failed."
     TESTS_FAILED=1
     PACKAGES_FAILED="${PACKAGES_FAILED} ${pkg}"
